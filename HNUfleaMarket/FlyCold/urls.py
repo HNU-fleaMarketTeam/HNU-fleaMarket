8aa9d9d92a224b2cf3d6080f2c77b337fe447207
袁其升 <90436981+mokie223@users.noreply.github.com>
2021-11-21 09:39:51 +0800

    Update urls.py

--- HNUfleaMarket/FlyCold/urls.py
+++ HNUfleaMarket/FlyCold/urls.py
@@ -19,3 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from SchoolBuy import urls
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += urls.urlpatterns
